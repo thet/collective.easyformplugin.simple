abb02cf360bcb32ece18f2056131c96677bf3688
Johannes Raggam <thetetet@gmail.com>
2020-05-28 17:56:46 +0200

    simpler easyform derivate

--- src/collective/easyformplugin/simple/content/simple_form.py
+++ src/collective/easyformplugin/simple/content/simple_form.py
@@ -1,60 +1,35 @@
-# -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+from collective.easyform.interfaces import IEasyForm
+from plone.autoform import directives
+
+
 from plone.dexterity.content import Container
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
 from zope.interface import implementer
 
 
-# from collective.easyformplugin.simple import _
-
-
-class ISimpleForm(model.Schema):
+class ISimpleForm(IEasyForm):
     """ Marker interface and Dexterity Python Schema for SimpleForm
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('simple_form.xml')
-
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    directives.omitted(
+        "submitLabel",
+        "useCancelButton",
+        "resetLabel",
+        "method",
+        "form_tabbing",
+        "default_fieldset_label",
+        "unload_protection",
+        "CSRFProtection",
+        "forceSSL",
+        # overrides
+        "thanksPageOverrideAction",
+        "thanksPageOverride",
+        "formActionOverride",
+        "onDisplayOverride",
+        "afterValidationOverride",
+        "headerInjection",
+        "submitLabelOverride",
+    )
 
 
 @implementer(ISimpleForm)
