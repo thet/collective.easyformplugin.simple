9434cfd34968a32a738eea7942c9d9601c1447e4
Johannes Raggam <thetetet@gmail.com>
2020-06-02 15:20:54 +0200

    rename ICollectiveEasyformpluginSimpleLayer to IBrowserLayer

--- src/collective/easyformplugin/simple/interfaces.py
+++ src/collective/easyformplugin/simple/interfaces.py
@@ -4,5 +4,5 @@
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
-class ICollectiveEasyformpluginSimpleLayer(IDefaultBrowserLayer):
+class IBrowserLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
--- src/collective/easyformplugin/simple/tests/test_setup.py
+++ src/collective/easyformplugin/simple/tests/test_setup.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
-from collective.easyformplugin.simple.testing import COLLECTIVE_EASYFORMPLUGIN_SIMPLE_INTEGRATION_TESTING  # noqa: E501
+from collective.easyformplugin.simple.interfaces import IBrowserLayer
+from collective.easyformplugin.simple.testing import (
+    COLLECTIVE_EASYFORMPLUGIN_SIMPLE_INTEGRATION_TESTING,
+)  # noqa: E501
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
+from plone.browserlayer import utils
 
 import unittest
 
@@ -21,25 +25,21 @@ class TestSetup(unittest.TestCase):
 
     def setUp(self):
         """Custom shared utility setup for tests."""
-        self.portal = self.layer['portal']
+        self.portal = self.layer["portal"]
         if get_installer:
-            self.installer = get_installer(self.portal, self.layer['request'])
+            self.installer = get_installer(self.portal, self.layer["request"])
         else:
-            self.installer = api.portal.get_tool('portal_quickinstaller')
+            self.installer = api.portal.get_tool("portal_quickinstaller")
 
     def test_product_installed(self):
         """Test if collective.easyformplugin.simple is installed."""
-        self.assertTrue(self.installer.isProductInstalled(
-            'collective.easyformplugin.simple'))
+        self.assertTrue(
+            self.installer.isProductInstalled("collective.easyformplugin.simple")
+        )
 
     def test_browserlayer(self):
-        """Test that ICollectiveEasyformpluginSimpleLayer is registered."""
-        from collective.easyformplugin.simple.interfaces import (
-            ICollectiveEasyformpluginSimpleLayer)
-        from plone.browserlayer import utils
-        self.assertIn(
-            ICollectiveEasyformpluginSimpleLayer,
-            utils.registered_layers())
+        """Test that IBrowserLayer is registered."""
+        self.assertIn(IBrowserLayer, utils.registered_layers())
 
 
 class TestUninstall(unittest.TestCase):
@@ -47,26 +47,22 @@ class TestUninstall(unittest.TestCase):
     layer = COLLECTIVE_EASYFORMPLUGIN_SIMPLE_INTEGRATION_TESTING
 
     def setUp(self):
-        self.portal = self.layer['portal']
+        self.portal = self.layer["portal"]
         if get_installer:
-            self.installer = get_installer(self.portal, self.layer['request'])
+            self.installer = get_installer(self.portal, self.layer["request"])
         else:
-            self.installer = api.portal.get_tool('portal_quickinstaller')
+            self.installer = api.portal.get_tool("portal_quickinstaller")
         roles_before = api.user.get_roles(TEST_USER_ID)
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        self.installer.uninstallProducts(['collective.easyformplugin.simple'])
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+        self.installer.uninstallProducts(["collective.easyformplugin.simple"])
         setRoles(self.portal, TEST_USER_ID, roles_before)
 
     def test_product_uninstalled(self):
         """Test if collective.easyformplugin.simple is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled(
-            'collective.easyformplugin.simple'))
+        self.assertFalse(
+            self.installer.isProductInstalled("collective.easyformplugin.simple")
+        )
 
     def test_browserlayer_removed(self):
-        """Test that ICollectiveEasyformpluginSimpleLayer is removed."""
-        from collective.easyformplugin.simple.interfaces import \
-            ICollectiveEasyformpluginSimpleLayer
-        from plone.browserlayer import utils
-        self.assertNotIn(
-            ICollectiveEasyformpluginSimpleLayer,
-            utils.registered_layers())
+        """Test that IBrowserLayer is removed."""
+        self.assertNotIn(IBrowserLayer, utils.registered_layers())
