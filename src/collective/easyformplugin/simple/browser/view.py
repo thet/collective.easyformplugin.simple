f843f8fb31187900fbb1516a23d55e48ebe3f318
Manuel Reinhardt <reinhardt@syslab.com>
2020-06-05 13:54:46 +0200

    Added Divider field, a custom label field that uses a different widget template; intended as an alternative to the use of fieldsets. Refs SCR-263

--- src/collective/easyformplugin/simple/browser/view.py
+++ src/collective/easyformplugin/simple/browser/view.py
@@ -2,7 +2,9 @@ from collective.easyform.browser.view import EasyFormForm
 from collective.easyform.browser.view import EasyFormFormWrapper
 from collective.easyform.interfaces import IEasyFormForm
 from collective.easyformplugin.simple.browser.widgets import ConsentFieldWidget
+from collective.easyformplugin.simple.browser.widgets import DividerFieldWidget
 from collective.easyformplugin.simple.fields import Consent
+from collective.easyformplugin.simple.fields import Divider
 from plone.autoform.widgets import ParameterizedWidget
 from z3c.form.browser.checkbox import CheckBoxWidget
 from z3c.form.browser.radio import RadioFieldWidget
@@ -17,6 +19,8 @@ class SimpleFormForm(EasyFormForm):
         for field in self.fields.values():
             if isinstance(field.field, Consent):
                 field.widgetFactory["input"] = ParameterizedWidget(ConsentFieldWidget)
+            elif isinstance(field.field, Divider):
+                field.widgetFactory["input"] = ParameterizedWidget(DividerFieldWidget)
             elif isinstance(field.field, Choice):
                 field.widgetFactory["input"] = ParameterizedWidget(RadioFieldWidget)
             elif isinstance(field.field, Set):
--- src/collective/easyformplugin/simple/content/simple_form.py
+++ src/collective/easyformplugin/simple/content/simple_form.py
@@ -1,7 +1,10 @@
+from collective.easyform import easyformMessageFactory as _  # NOQA
 from collective.easyform.interfaces import IEasyForm
+from plone import api
 from plone.autoform import directives
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from zope.i18n import translate
 from zope.interface import implementer
 
 
@@ -34,3 +37,7 @@ class ISimpleForm(IEasyForm):
 class SimpleForm(Container):
     """
     """
+
+    @property
+    def submitLabel(self):
+        return translate(_(u"default_submitLabel", u"Submit"))
