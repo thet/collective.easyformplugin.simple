a7901a5dbf458ee1d6d73b299e368cddcc963a9e
Manuel Reinhardt <reinhardt@syslab.com>
2020-06-05 12:21:25 +0200

    Better choice of widgets. Refs SCR-263

---
+++ src/collective/easyformplugin/simple/browser/view.py
@@ -0,0 +1,31 @@
+from collective.easyform.browser.view import EasyFormForm
+from collective.easyform.browser.view import EasyFormFormWrapper
+from collective.easyform.interfaces import IEasyFormForm
+from collective.easyformplugin.simple.browser.widgets import ConsentFieldWidget
+from collective.easyformplugin.simple.fields import Consent
+from plone.autoform.widgets import ParameterizedWidget
+from z3c.form.browser.checkbox import CheckBoxWidget
+from z3c.form.browser.radio import RadioFieldWidget
+from zope.interface import implementer
+from zope.schema import Choice
+from zope.schema import Set
+
+
+@implementer(IEasyFormForm)
+class SimpleFormForm(EasyFormForm):
+    def updateWidgets(self):
+        for field in self.fields.values():
+            if isinstance(field.field, Consent):
+                field.widgetFactory["input"] = ParameterizedWidget(ConsentFieldWidget)
+            elif isinstance(field.field, Choice):
+                field.widgetFactory["input"] = ParameterizedWidget(RadioFieldWidget)
+            elif isinstance(field.field, Set):
+                field.widgetFactory["input"] = ParameterizedWidget(CheckBoxWidget)
+        super(SimpleFormForm, self).updateWidgets()
+
+
+class SimpleFormFormWrapper(EasyFormFormWrapper):
+    form = SimpleFormForm
+
+
+SimpleFormView = SimpleFormFormWrapper
